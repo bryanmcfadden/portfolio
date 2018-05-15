d9bbdc4251b5cdb46bd027e55d3a9a8a0031597d
Bryan McFadden <bryan.mcfadden@gmail.com>
2018-05-15 17:04:48 -0400

    Fixed setTimeout error with resume buttons

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,41 +1,43 @@
+
+
 $(document).ready(function(){
 
+  var timerResumeButtons = {
+    start: function(){
+      if (typeof this.timeoutID === 'number'){
+        this.stop();
+      }
+      this.timeoutID = setTimeout(function(){DisplayResumeButtons(false);},15000);
+    },
+    stop: function(){
+      window.clearTimeout(this.timeoutID);
+      this.timeoutID = undefined;
+    }
+  }
+
+ // about me
   $('#btnViewResume').on('click', function(){
     $('#btnViewResume, #aboutArea h4').fadeOut(500, function(){
       DisplayResumeButtons(true);
     });
   });
 
+
   $('.resume').bind({
     mouseenter: function(e) {
-      clearTimeout(timerResumeButtons);
+      timerResumeButtons.stop();
     },
     mouseleave: function(e) {
-    timerResumeButtons = setTimeout(function(){DisplayResumeButtons(false);},5000);
-    //  alert(timerResumeButtons);
+      timerResumeButtons.start();
     }
   });
-  /*
-  $('.resume').on("mouseover", function(){
-    clearTimeout(viewResumeButtons);
-    alert(viewresumeButtons);
-  });
 
-  $('a.resume').on("mouseout", function(){
-    alert(viewResumebuttons);
-    viewResumeButtons = setTimeout(function(){
-      DisplayResumeButtons(false);
-    },
-    15000
-    );
-  });
-*/
-  var timerResumeButtons;
 
   function DisplayResumeButtons(val){
     if(val){
       // start timer, hide button, change test and show doc/pdf buttons
-      timerResumeButtons = setTimeout(function(){DisplayResumeButtons(false);},5000);
+      //timerResumeButtons = setTimeout(function(){DisplayResumeButtons(false);},5000);
+      timerResumeButtons.start();
       $('.resume').css({"width":"60px", "visibility":"visible"});
       $('.resume-doc').css("margin-right", "15px");
       $('.resume-pdf').css("margin-left", "80px");
@@ -45,7 +47,7 @@ $(document).ready(function(){
       $('#aboutArea h4').fadeIn(500);
     }else{
       // hide doc/pdf buttons, change test and show view resume button
-      clearTimeout(timerResumeButtons);
+      timerResumeButtons.stop();
       $('.resume').animate({"opacity":"0", "margin-left":"0"}, 1000, function(){});
       $('.resume').css({"width":"1px", "visibility":"hidden"});
       $('.resume-doc').css("margin-right", "0px");
@@ -56,4 +58,5 @@ $(document).ready(function(){
       });
     }
   }
+
 });
